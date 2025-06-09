dc50c62a214c9251206ec61d3042c82709b6088b
Aman Kumar <aman.kumar01747@gmail.com>
2025-06-09 12:41:35 +0530

    -Added drag & drop feature to instantiate the monkey

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector3 originalPosition;
+        private Vector3 originalAnchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -22,6 +25,35 @@ namespace ServiceLocator.UI
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            rectTransform = GetComponent<RectTransform>();
+            originalPosition = rectTransform.position;
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1f, 1f, 1f, 1f);
+            rectTransform.position = originalPosition;
+            rectTransform.anchoredPosition = originalAnchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1f, 1f, 1f, 0.6f); // Set transparency to 50% on pointer down
         }
     }
 }
