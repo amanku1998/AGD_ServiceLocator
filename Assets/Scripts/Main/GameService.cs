8d72b5e14b2d7b9248bfacbad96f78a6329758ef
Aman Kumar <aman.kumar01747@gmail.com>
2025-06-13 20:11:27 +0530

    -Fix drag & drop issue also apply DI in remaining script

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -9,18 +9,17 @@ using ServiceLocator.UI;
 
 namespace ServiceLocator.Main
 {
-    public class GameService : GenericMonoSingleton<GameService>
+    public class GameService : MonoBehaviour
     {
         // Services:
-        public EventService EventService { get; private set; }
-        public MapService MapService { get; private set; }
-        public WaveService WaveService { get; private set; }
-        public SoundService SoundService { get; private set; }
-        public PlayerService PlayerService { get; private set; }
+        private EventService eventService;
+        private MapService mapService;
+        private WaveService waveService ;
+        private SoundService soundService;
+        private PlayerService playerService;
 
         [SerializeField] private UIService uiService;
-        public UIService UIService => uiService;
-
+        private UIService UIService => uiService;
 
         // Scriptable Objects:
         [SerializeField] private MapScriptableObject mapScriptableObject;
@@ -40,24 +39,24 @@ namespace ServiceLocator.Main
 
         private void createServices()
         {
-            EventService = new EventService();
-            MapService = new MapService(mapScriptableObject);
-            WaveService = new WaveService(waveScriptableObject);
-            SoundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
-            PlayerService = new PlayerService(playerScriptableObject);
+            eventService = new EventService();
+            mapService = new MapService(mapScriptableObject);
+            waveService = new WaveService(waveScriptableObject);
+            soundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
+            playerService = new PlayerService(playerScriptableObject);
         }
 
         private void InjectServiceDependencies()
         {
-            PlayerService.Init(UIService, MapService, SoundService);
-            WaveService.Init(EventService, UIService, MapService, SoundService, PlayerService);
-            MapService.Init(EventService);
-            UIService.Init(EventService , WaveService);
+            playerService.Init(UIService, mapService, soundService);
+            waveService.Init(eventService, UIService, mapService, soundService, playerService);
+            mapService.Init(eventService);
+            UIService.Init(eventService, waveService, playerService);
         }
 
         private void Update()
         {
-            PlayerService.Update();
+            playerService.Update();
         }
     }
 }
--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -6,6 +6,7 @@ using ServiceLocator.Main;
 using UnityEngine.SceneManagement;
 using ServiceLocator.Events;
 using ServiceLocator.Wave;
+using ServiceLocator.Player;
 
 namespace ServiceLocator.UI
 {
@@ -22,6 +23,7 @@ namespace ServiceLocator.UI
         [Header("Level Selection Panel")]
         [SerializeField] private GameObject levelSelectionPanel;
         [SerializeField] private Button Map1Button;
+        [SerializeField] private MapButton mapButton;
 
         [Header("Monkey Selection UI")]
         private MonkeySelectionUIController monkeySelectionController;
@@ -38,12 +40,11 @@ namespace ServiceLocator.UI
 
         private EventService eventService;
         private WaveService waveService;
+        private PlayerService playerService;
 
         private void Start()
         {
-            monkeySelectionController = new MonkeySelectionUIController(cellContainer, monkeyCellPrefab, monkeyCellScriptableObjects);
             MonkeySelectionPanel.SetActive(false);
-            monkeySelectionController.SetActive(false);
 
             gameplayPanel.SetActive(false);
             levelSelectionPanel.SetActive(true);
@@ -54,12 +55,20 @@ namespace ServiceLocator.UI
             playAgainButton.onClick.AddListener(OnPlayAgainButtonClicked);
         }
 
-        public void Init(EventService eventService, WaveService waveService)
+        private void InitializeMapSelectionUI()
+        {
+            monkeySelectionController = new MonkeySelectionUIController(cellContainer, monkeyCellPrefab, monkeyCellScriptableObjects, playerService);
+            monkeySelectionController.SetActive(false);
+        }
+
+        public void Init(EventService eventService, WaveService waveService, PlayerService playerService)
         {
             this.eventService = eventService;
             this.waveService = waveService;
-
+            this.playerService = playerService;
+            mapButton.Init(eventService);
             SubscribeToEvents();
+            InitializeMapSelectionUI();
         }
 
         public void SubscribeToEvents() => eventService.OnMapSelected.AddListener(OnMapSelected);
